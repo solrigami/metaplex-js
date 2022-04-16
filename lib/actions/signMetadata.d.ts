610de00b5d94daf4da3cfdfaebfe9dcb147b9f4d
Welison Almeida <welison.almeida.923@gmail.com>
2022-04-16 01:01:42 -0300

    Remove lib folder from solrigami-custom branch

--- lib/actions/signMetadata.d.ts
+++
@@ -1,9 +0,0 @@
-import { Connection, Keypair, PublicKey } from '@solana/web3.js';
-import { Wallet } from '../wallet';
-export interface SignMetadataParams {
-    connection: Connection;
-    wallet: Wallet;
-    editionMint: PublicKey;
-    signer?: Keypair;
-}
-export declare const signMetadata: ({ connection, wallet, editionMint, signer }?: SignMetadataParams) => Promise<string>;
